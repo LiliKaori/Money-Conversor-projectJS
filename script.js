02fa1788cf6fbc6b5d8d9f16bfd1c67330c1a165
LiliKaori <lilikaori10@gmail.com>
2023-03-14 13:53:16 -0300

    Adicionar API e aumentar moedas

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const button = document.getElementById("convertButton")
 const select = document.getElementById("currencySelect")
 
-const dollar = 5.2
-const euro = 5.9
-const bitcoin = 121620.17
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById("inputReal").value
     const realValueText = document.getElementById("RealValueText")
     const currencyValueText = document.getElementById("currencyValueText")
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,CAD-BRL,ARS-BRL,JPY-BRL,CNY-BRL").then(response=> response.json())
+    console.log(data)
+
+    const dollar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+    const dollarC = data.CADBRL.high
+    const peso = data.ARSBRL.high
+    const yen = data.JPYBRL.high
+    const yuan = data.CNYBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(inputReais)
 
@@ -22,6 +29,22 @@ const convertValues = () => {
 
     if (select.value == "BTC Bitcoin") {
         currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BTC' }).format(inputReais / bitcoin)
+    }
+
+    if (select.value == "CA$ Canadian Dollar") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'CAD' }).format(inputReais / dollarC)
+    }
+
+    if (select.value == "ARS Argentine Peso") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'ARS' }).format(inputReais / peso)
+    }
+
+    if (select.value == "JP¥ Yen") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'JPY' }).format(inputReais / yen)
+    }
+
+    if (select.value == "CN¥ Yuan") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'CNY' }).format(inputReais / yuan)
     }
 }
 
@@ -40,6 +63,22 @@ const changeCurrency = () => {
     if (select.value == "BTC Bitcoin") {
         currencyName.innerHTML = "Bitcoin"
         currencyImg.src = "./Images/BTC_icon.png"
+    }
+     if (select.value == "CA$ Canadian Dollar") {
+        currencyName.innerHTML = "Canadian Dollar"
+        currencyImg.src = "./Images/CAD_icon.png"
+    }
+     if (select.value == "ARS Argentine Peso") {
+        currencyName.innerHTML = "Argentine Peso"
+        currencyImg.src = "./Images/ARG_icon.png"
+    }
+     if (select.value == "JP¥ Yen") {
+        currencyName.innerHTML = "Yen"
+        currencyImg.src = "./Images/JPY_icon.png"
+    }
+     if (select.value == "CN¥ Yuan") {
+        currencyName.innerHTML = "Yuan"
+        currencyImg.src = "./Images/China_icon.png"
     }
 
     convertValues()
